4c7446df051f2c62df505cecdefa00a1959f6e90
Colin Combe <colin.combe@ed.ac.uk>
2018-02-22 11:07:47 +0000

    synching up with lars

--- parser.py
+++ parser.py
@@ -25,7 +25,7 @@ try:
         dname = ''
 
     # import local files
-    import xiUI_mzid as mzidParser
+    import xiSPEC_mzid as mzidParser
     import xiSPEC_csv as csvParser
     from xiSPEC_peakList import unzip_peak_lists
 
@@ -38,14 +38,14 @@ try:
         dev = True
         logFile = "log/parser_%s.log" % int(time())
 
-    # try:
-    #     os.remove(logFile)
-    # except OSError:
-    #     pass
-    # os.fdopen(os.open(logFile, os.O_WRONLY | os.O_CREAT, 0o777), 'w').close()
+    try:
+        os.remove(logFile)
+    except OSError:
+        pass
+    os.fdopen(os.open(logFile, os.O_WRONLY | os.O_CREAT, 0o777), 'w').close()
 
     # create logger
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(filename=logFile, level=logging.DEBUG,
                         format='%(asctime)s %(levelname)s %(name)s %(message)s')
     logger = logging.getLogger(__name__)
 
@@ -59,7 +59,7 @@ try:
     else:
         import xiSPEC_sqlite as db
 except IndexError:
-    import xiUI_pg as db
+    import xiSPEC_sqlite as db
 
 
 returnJSON = {
@@ -78,7 +78,7 @@ try:
 
     # development testfiles
     if dev:
-        baseDir = "/home/col/xiSPEC_test_files/"
+        baseDir = "/media/data/work/xiSPEC_test_files/"
         # identifications_file = baseDir + 'OpenxQuest_example_added_annotations.mzid'
         # peakList_file = baseDir + "centroid_B170808_08_Lumos_LK_IN_90_HSA-DSSO-Sample_Xlink-CID-EThcD.mzML"
         # peakList_file = baseDir + "B170918_12_Lumos_LK_IN_90_HSA-DSSO-HCD_Rep1.mgf"
@@ -94,7 +94,7 @@ try:
         identifications_file = baseDir + "DSSO_B170808_08_Lumos_LK_IN_90_HSA-DSSO-Sample_Xlink-CID-EThcD_CID-only.mzid"
         peakList_file = baseDir + "centroid_B170808_08_Lumos_LK_IN_90_HSA-DSSO-Sample_Xlink-CID-EThcD.mzML"
 
-        # large mzid dataset
+        # # large mzid dataset
         # identifications_file = baseDir + "Tmuris_exo/Tmuris_exosomes1.mzid"
         # peakList_file = baseDir + "Tmuris_exo/20171027_DDA_JC1.zip"
 
