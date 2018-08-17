38a8a0d5a3c09bd0c289ffc7f3119d24e07b28b1
Colin <colin.combe@ed.ac.uk>
2018-08-17 15:38:20 +0100

    problem with json encoder

--- MzIdParser.py
+++ MzIdParser.py
@@ -880,7 +880,16 @@ class xiSPEC_MzIdParser(MzIdParser):
 
 
 class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
+    # def default(self, obj):
+    #     if isinstance(obj, np.ndarray):
+    #         return obj.tolist()
+    #     return json.JSONEncoder.default(self, obj)
+    def default(self, o):
+        try:
+            iterable = iter(o)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, o)
