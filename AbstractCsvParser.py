e4d5441b1fbbef9498c48be7321a9204e0ef3e5b
Colin <colin.combe@ed.ac.uk>
2018-12-05 13:29:32 +0000

    fix for csv files

--- AbstractCsvParser.py
+++ AbstractCsvParser.py
@@ -246,13 +246,13 @@ class AbstractCsvParser:
         self.logger.info('reading fasta - done. Time: ' + str(round(time() - self.start_time, 2)) + " sec")
 
     def upload_info(self):
-       self.logger.info('new csv upload')
-       ident_file_size = os.path.getsize(self.csv_path)
-       # peak_list_file_names = json.dumps(self.get_peak_list_file_names(), cls=NumpyEncoder)
-       self.upload_id = self.db.new_upload([self.user_id, os.path.basename(self.csv_path), "", ident_file_size],
+        self.logger.info('new csv upload')
+        # ident_file_size = os.path.getsize(self.csv_path)
+        # peak_list_file_names = json.dumps(self.get_peak_list_file_names(), cls=NumpyEncoder)
+        self.upload_id = self.db.new_upload([self.user_id, os.path.basename(self.csv_path), "-"],
                        self.cur, self.con,
                        )
-       self.random_id = self.db.get_random_id(self.upload_id, self.cur, self.con)
+        self.random_id = self.db.get_random_id(self.upload_id, self.cur, self.con)
 
 
 # class NumpyEncoder(json.JSONEncoder):
